a24cd2435db34e41c95dd9d46922e7a1a953bce2
sachin <ssgomtiwal@gmail.com>
2022-09-20 22:58:39 +0530

    code fix by stranger1

--- prime.js
+++ prime.js
@@ -1,10 +1,20 @@
 function checkprime(num){
 
-    for(let i=2;i<=Math.sqrt(num);i++){
+    let count=0;
+    for(let i=0;i<=num;i++){
         if(num%i==0){
-            return false;
+            count++;
         }
-    } 
-        return true;
-}
-   console.log(checkprime(13))
+    }  
+     if(count==2){
+        return true
+     }else{
+        return false
+     }}
+        
+     let ans=checkprime(12)
+      if(ans=="true"){
+        console.log("Prime")
+      }else{
+        console.log("Not Pirme")
+      }
